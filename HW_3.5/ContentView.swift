48b553dbc0777c78ba6f4dc44170962f408d0818
Maxim Mitin <maximus405@gmail.com>
2021-09-22 20:45:12 +0300

    Initial Commit

---
+++ HW_3.5/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HW_3.5
+//
+//  Created by Maxim Mitin on 22.09.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HW_3.5/HW_3_5App.swift
@@ -0,0 +1,17 @@
+//
+//  HW_3_5App.swift
+//  HW_3.5
+//
+//  Created by Maxim Mitin on 22.09.21.
+//
+
+import SwiftUI
+
+@main
+struct HW_3_5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
